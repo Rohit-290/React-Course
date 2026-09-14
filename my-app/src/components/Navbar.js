8219d8260593b0b00867b5ed1c85af79c35ee5f0
Rohit-290 <rohit290kumar@gmail.com>
2026-09-14 09:39:40 +0530

    Udated on 14th September 2026

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
@@ -8,9 +9,9 @@ export default function Navbar(props) {
         data-bs-theme = {props.mode}
       >
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            {props.title}
-          </a>
+         <Link className="navbar-brand" to="/">
+           {props.title}
+         </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -25,14 +26,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
+               <Link className="nav-link" to="/">
+                 Home
+               </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
-                  {props.about}
-                </a>
+               <Link className="nav-link" to="/about">
+                  About
+               </Link>
               </li>
             </ul>
             {/* <form className="d-flex" role="search">
